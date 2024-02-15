3bee6f89c5bf85616adecb4fd5104c117df7a63f
PontusPilatus <paulsson.pontus@gmail.com>
2024-02-15 19:40:17 +0100

    added more features such as humidity and wind.

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './WeatherDisplay.css';
 
 function WeatherDisplay({ weather }) {
-  
+
+  const roundTemp = (temp) => Math.round(temp);
+
   return (
     <div>
       {weather ? (
         <div>
           <h2>{weather.name}</h2>
-          <p>{weather.main.temp}°C</p>
-          <p>{weather.weather[0].main}</p>
+          <p>Temperature: {roundTemp(weather.main.temp)}°C</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>Wind Speed: {weather.wind.speed} km/h</p>
+          <p>Conditions: {weather.weather[0].description}</p>
         </div>
       ) : (
         <p>No weather data available</p>
